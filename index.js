e20d244e64858d9473407541a32a3f0df8bd5801
Anthony Wong <anthony.wong182@gmail.com>
2018-04-22 14:59:37 -0700

    convert array in obj with data,size,time,lines

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { Transform } = require('stream')
 const fs = require('fs')
 // const fileName = process.argv[2]
 
-var offset = 0
+// START TIME (MS)
+const hrstart = process.hrtime()
+
+// READ STREAM AND TRANSFORM BUFFER
+// PIPE TO NEXT STREAM
 
 const lineSplit = new Transform({
   readableObjectMode: true,
@@ -14,12 +18,52 @@ const lineSplit = new Transform({
   }
 })
 
+// RECEIVE ARRAY AND CONVERT TO OBJECT
+// PROVIDE DATA, SIZE, TIME, AND LINES
+
+const arrayToObj = new Transform({
+  readableObjectMode: true,
+  writableObjectMode: true,
+
+  transform(chunk, encoding, callback) {
+    // CONSTRUCTOR
+    function Object(data, size, time, lines) {
+      this.data = data,
+        this.size = size,
+        this.time = time,
+        this.lines = lines
+    }
+
+    let tempArr = []
+
+    for (let i = 0; i < chunk.length; i++) {
+      let hrend = process.hrtime(hrstart)
+      // DATA FOR EACH OBJECT
+      const obj = new Object(
+        chunk[i],
+        Buffer.byteLength(chunk[i], 'utf8'),
+        hrend[1] / 1000000,
+        (chunk[i].match(/\n/g)) ? chunk[i].match(/\n/g).length + 1 : 1
+      )
+      tempArr.push(obj)
+    }
+    console.log(tempArr)
+    this.push(tempArr)
+    callback()
+  }
+})
+
+const objToString = new Transform({
+  transform(chunk, encoding, callback) {
+
+  }
+})
+
 process.stdin.on('readable', () => {
   let buf = process.stdin.read()
   if (!buf) return
-  for (; offset < buf.length; offset++) {
+  for (let offset = 0; offset < buf.length; offset++) {
     if (buf[offset] === 0x0a) {
-      console.dir(buf.slice(0, offset).toString())
       buf = buf.slice(offset + 1)
       offset = 0
       process.stdin.pipe(buf)
@@ -29,3 +73,5 @@ process.stdin.on('readable', () => {
   process.stdin.pipe(buf)
 })
   .pipe(lineSplit)
+  .pipe(arrayToObj)
+  // .pipe(process.stdout)
